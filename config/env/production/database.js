21e4928c9d7e09c502710fe09dbe51f6ef483978
Marcio <marciodosanjos@ymail.com>
2023-11-11 15:54:55 -0300

    env vars

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,21 @@
 // path: ./config/env/production/database.js
 
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
-  return {
-    connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'mysql',
+          host: env('DATABASE_HOST', '108.179.253.20'),
+          port: env.int('DATABASE_PORT', 3306),
+          database: env('DATABASE_NAME', 'onarra45_strapi'),
+          username: env('DATABASE_USERNAME', 'onarra45_strapi'),
+          password: env('DATABASE_PASSWORD', 'M7902@312a'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        options: {}
       },
-      debug: false,
     },
-  }
-};
+  });
+  
