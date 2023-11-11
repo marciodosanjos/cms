2dfeb3687bff7ccbb63a87959525f05f6ac2d4ea
Marcio <marciodosanjos@ymail.com>
2023-11-11 16:16:40 -0300

    ajuste chaves

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
-  url: "/dashboard",
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,17 +1,18 @@
-// Path: ./config/env/production/server.js
-// starting from Strapi v 4.6.1 server.js has to be the following
-
 module.exports = ({ env }) => ({
   proxy: true,
   host: "0.0.0.0",
-  port: process.env.PORT,
+  port: env('PORT'),
   url: env('MY_HEROKU_URL'),
   app: { 
-    keys: env.array('APP_KEYS')
+    keys: env.array('APP_KEYS'),
   },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
   },
-})
+  keys: env.array('APP_KEYS'), // Certifique-se de adicionar as chaves do aplicativo aqui também
+  jwt: {
+    secret: env('JWT_SECRET', '?efbrer3435wd201'),
+  },
+});
